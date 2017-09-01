3b34d20c7893ecf2b05e30e1b25b5cfbf7dd6cc8
Dennis George <dennispg@gmail.com>
2017-09-01 10:37:18 -0500

    made extension methods options to appease TypeScript tracers

--- sharepoint.d.ts
+++ sharepoint.d.ts
@@ -12,7 +12,7 @@ declare interface IEnumerable<T> {
 
     /** Execute a callback for every element in the matched set.
     @param callback The function that will called for each element, and passed an index and the element itself */
-    each(callback: (index?: number, item?: T) => void): void;
+    each?(callback: (index?: number, item?: T) => void): void;
 
     /**
      * Creates an array of values by running each element in collection through iteratee.
@@ -20,17 +20,17 @@ declare interface IEnumerable<T> {
      * @param iteratee The function invoked per iteration.
      * @return Returns the new mapped array.
      */
-    map<TResult>(iteratee: (item?: T, index?: number) => TResult): TResult[];
+    map?<TResult>(iteratee: (item?: T, index?: number) => TResult): TResult[];
 
     /** Converts a collection to a regular JS array. */
-    toArray(): T[];
+    toArray?(): T[];
 
 
     /** Returns the first element in the collection or null if none
      * @param iteratee An optional function to filter by
      * @return Returns the first item in the collection
      */
-    firstOrDefault(iteratee?: (item?: T) => boolean): T;
+    firstOrDefault?(iteratee?: (item?: T) => boolean): T;
 }
 
 declare namespace SP {
